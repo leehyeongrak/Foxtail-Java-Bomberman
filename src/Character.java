ee059476bd2b1c5596623b73e344aad072bf5857
hyeongrak <leehrak@gmail.com>
2017-11-03 16:44:18 +0900

    character 생성

--- src/Character.java
+++ src/Character.java
@@ -1,10 +1,27 @@
+import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Character {
     int x, y;
+    PImage characterImage[];
 
-
-    public Character(PImage characterImage) {
-        super();
+    public Character(PImage characterImage[], int x, int y) {
+        this.characterImage = characterImage;
+        this.x = x;
+        this.y = y;
     }
+
+
+    int tick = 0;
+    int direction = 0;
+    boolean isMoving;
+
+    public void draw(PApplet pApplet) {
+        tick ++;
+//            pApplet.image(characterImage[tick / 10 % 5 + direction], x+8, y);
+//            System.out.println(tick / 10 % 5 + direction);
+            pApplet.image(characterImage[tick / 10 % 3 + direction], x+8, y);
+            System.out.println(tick / 10 % 3 + direction);
+    }
+
 }
--- src/Program.java
+++ src/Program.java
@@ -8,11 +8,12 @@ public class Program extends PApplet{
     @Override
     public void settings() {
         this.size(800, 600);
+        System.out.println("Settings");
     }
 
     int map[][] = {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1},
+            {1, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 1},
             {1, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 3, 1},
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
             {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
@@ -24,14 +25,11 @@ public class Program extends PApplet{
             {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
             {1, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 3, 1},
-            {1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1},
+            {1, 8, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 9, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
 
 
-
-//    PImage spriteStay;
-//    PImage[] spriteCharStay;
 
     PImage stoneImage;
     PImage spriteStoneImage;
@@ -39,31 +37,25 @@ public class Program extends PApplet{
     PImage boxImage;
     PImage spriteBoxImage;
 
+    PImage characterStayImage;
+    PImage[] spriteCharacterStayImage;
+
+    PImage characterMoveImage;
+    PImage[] spriteCharacterMoveImage;
+
     private List<Block> stones = new ArrayList<>();
     private List<Block> boxes = new ArrayList<>();
-
+    private Character character1p;
 
     @Override
     public void setup() {
         this.background(0);
         System.out.println("Setup");
 
-
-
-
-
-//        spriteStay = this.loadImage("bomberman-stay.png");
-//
-//        spriteCharStay = new PImage[12];
-//
-//        for(int i = 0 ; i < 3; i ++){
-//            for( int j = 0 ; j < 4 ; j ++){
-//                spriteCharStay[j * 3  + i] = spriteStay.get(20 * i, 32 * j, 20, 32);
-//            }
-//        }
-
         stoneSetup();
         boxSetup();
+        characterSetup();
+        movementSetup();
     }
 
 
@@ -77,6 +69,8 @@ public class Program extends PApplet{
         for (Block e : boxes) {
             e.draw(this);
         }
+
+        character1p.draw(this);
     }
 
     public static void main(String[] args) {
@@ -112,4 +106,66 @@ public class Program extends PApplet{
         }
     }
 
+    private void characterSetup(){
+        characterStayImage = this.loadImage("bomberman-stay.png");
+
+        spriteCharacterStayImage = new PImage[12];
+
+        for(int i = 0 ; i < 3; i ++){
+            for( int j = 0 ; j < 4 ; j ++){
+                spriteCharacterStayImage[j * 3  + i] = characterStayImage.get(20 * i, 32 * j, 20, 32);
+            }
+        }
+
+        for(int i = 0; i < 15; i ++){
+            for(int j = 0; j < 20; j++){
+                int randomNumber = (int)(Math.random()*4) + 6;
+                if(map[i][j] == randomNumber){
+                    character1p = new Character(spriteCharacterStayImage, j*40, i*40);
+                }
+            }
+        }
+    }
+
+    private void movementSetup(){
+        characterMoveImage = this.loadImage("bomberman-movement.png");
+
+        spriteCharacterMoveImage = new PImage[20];
+
+        for(int i = 0 ; i < 5; i++) {
+            for (int j = 0; j <4; j++) {
+                spriteCharacterMoveImage[j * 5  + i] = characterStayImage.get(20 * i, 32 * j, 20, 32);
+            }
+        }
+    }
+
+    @Override
+    public void keyPressed() {
+//        character1p.characterImage = spriteCharacterMoveImage;
+        if(keyCode == 37){
+            character1p.direction = 9;
+//            character1p.isMoving = true;
+            character1p.x -= 5;
+//            System.out.println("left");
+        }
+        if(keyCode == 38){
+            character1p.direction = 6;
+            character1p.y -= 5;
+//            System.out.println("up");
+        }
+        if(keyCode == 39){
+            character1p.direction = 3;
+            character1p.x += 5;
+//            System.out.println("right");
+        }
+        if(keyCode == 40){
+            character1p.direction = 0;
+            character1p.y += 5;
+//            System.out.println("down");
+        }
+    }
+
+    @Override
+    public void keyReleased() {
+    }
 }
